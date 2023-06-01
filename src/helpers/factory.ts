e5cb7bc88d49f64f4082bd642e6af21de46f0639
Innei <tukon479@gmail.com>
2023-06-01 12:46:38 +0800

    fix: url with protocol
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/helpers/factory.ts
+++ src/helpers/factory.ts
@@ -1,29 +1,31 @@
-import { NextRequest, NextResponse } from "next/server";
-import { withQuery } from "ufo";
+import { NextRequest, NextResponse } from 'next/server'
+import { withQuery } from 'ufo'
 
 export const createHandler = (region: string) => {
   return async (req: NextRequest) => {
-    const body = await req.json();
-    const { url } = body;
-    if (!url) return;
+    const body = await req.json()
+    const { url } = body as { url: string }
+    if (!url) return
 
     try {
-      const startAt = Date.now();
+      const startAt = Date.now()
 
-      await fetch(withQuery(url, { __pong: startAt }));
+      const urlWithProtocol = url.startsWith('http') ? url : `https://${url}`
+
+      await fetch(withQuery(urlWithProtocol, { __pong: startAt }))
 
       return NextResponse.json({
         region,
         duration: Date.now() - startAt,
         code: 200,
-      });
+      })
     } catch (error) {
-      const err = error as { statusCode: number };
+      const err = error as { statusCode: number }
       return NextResponse.json({
         region,
         duration: -1,
         code: err.statusCode,
-      });
+      })
     }
-  };
-};
+  }
+}
