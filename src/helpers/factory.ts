5965e27ae520586c3cc27a5a129606a84cb75ecc
Innei <tukon479@gmail.com>
2023-06-14 09:23:53 +0800

    fix: url with protocol (#2)
    Co-authored-by: Cuvii <xw5427@gmail.com>

--- src/helpers/factory.ts
+++ src/helpers/factory.ts
@@ -1,16 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { withQuery } from 'ufo'
+import { withHttps, withQuery } from 'ufo'
 
 export const createHandler = (region: string) => {
   return async (req: NextRequest) => {
     const body = await req.json()
-    const { url } = body
+    const { url } = body as { url: string }
     if (!url) return
 
     try {
       const startAt = Date.now()
 
-      await fetch(withQuery(url, { __pong: startAt }))
+      await fetch(withQuery(withHttps(url), { __pong: startAt }))
 
       return NextResponse.json({
         region,
