10524c67d25bcdbd2307cf351d026c40f6b4b9cf
Cuvii <xw5427@gmail.com>
2023-07-03 22:46:27 +0800

    feat: components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Card from '../components/Card.svelte';
+</script>
+
 <div class="mx-auto flex max-w-md justify-center">
 	<input type="text" required={false} placeholder="Enter your URL" />
 	<button class="ml-4"> Pong! </button>
